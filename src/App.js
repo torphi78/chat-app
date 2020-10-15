ac7c83bb276ce1ab6405a8bcdb80e7b6f8b043bc
wilder <torphi78@gmail.com>
2020-10-15 12:48:09 +0200

    Créer et afficher un composant ContactList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
+import ContactList from './components/ContactList';
+import './components/Contact.css';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Contact/>
+      
+      <ContactList />
+      
     </div>
   );
 }
